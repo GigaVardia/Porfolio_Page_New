556b0ebe126e838968aa2f74f35938ee11b8712f
GigaVardia <gigavardia@gmail.com>
2021-08-30 06:26:15 +0400

    First Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container} from "@material-ui/core";
+import {FacebookIco, GitHubIco, LinkedInIco, ReactIco} from "./Helpers/FontAwesome";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-inner container">
+                <Container fixed>
+
+                </Container>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/Components/Helpers/FontAwesome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithub, faLinkedinIn, faFacebook, faReact } from '@fortawesome/free-brands-svg-icons'
+
+const GitHubIco = <FontAwesomeIcon icon={faGithub} />
+const LinkedInIco = <FontAwesomeIcon icon={faLinkedinIn}/>
+const FacebookIco = <FontAwesomeIcon icon={faFacebook}/>
+const ReactIco = <FontAwesomeIcon icon={faReact}/>
+
+export {
+    GitHubIco,
+    LinkedInIco,
+    FacebookIco,
+    ReactIco
+}
